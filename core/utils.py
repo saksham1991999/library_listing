c20a337a3205769a4dccc4037ffcbbdebeb58013
Saksham Mittal <saksham1991999@users.noreply.github.com>
2020-10-14 18:42:58 +0530

    updated geoip location function

---
+++ core/utils.py
@@ -0,0 +1,16 @@
+from django.contrib.gis.geoip2 import GeoIP2
+
+# Helper functions
+
+def get_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    g = GeoIP2()
+    country = g.country(ip)
+    city = g.city(ip)
+    lat, lon = g.lat_lon(ip)
+    return country, city, lat, lon
+
